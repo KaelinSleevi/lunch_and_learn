23dbfe34c36585eb357dda2e2d309a2f67e37316
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-11-14 12:00:28 -0700

    feat: stuck on changing params.

--- app/controllers/api/v1/tourist_sights_controller.rb
+++ app/controllers/api/v1/tourist_sights_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::TouristSightsController < ApplicationController
 
  def index
+  country = RestFacade.country_details
   @places = PlaceFacade.tourism_details(params[:lon], params[:lat])
 
   render json: PlaceSerializer.new(@places)
