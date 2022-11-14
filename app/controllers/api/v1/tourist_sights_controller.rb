d1a3d86713ab4d1a4b0cf2dc44b79c92edf7d697
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-11-14 12:10:55 -0700

    final assessment time

--- app/controllers/api/v1/tourist_sights_controller.rb
+++ app/controllers/api/v1/tourist_sights_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::TouristSightsController < ApplicationController
 
  def index
-  country = RestFacade.country_details
+  country = RestFacade.country_details(params[:country])
   @places = PlaceFacade.tourism_details(params[:lon], params[:lat])
 
   render json: PlaceSerializer.new(@places)
