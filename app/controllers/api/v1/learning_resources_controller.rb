4ca3c18ca0b44992dccad5021ca1d22a34c294d7
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-11-14 16:07:06 -0700

    add initial service call in controller to youtube api.

--- app/controllers/api/v1/learning_resources_controller.rb
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::LearningResourcesController < ApplicationController
  def index
-  
+  response = Faraday.get("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCluQ5yInbeAkkeCndNnUhpw&q=China&key=AIzaSyDdWt8K3a4Ilr5Qwc81U6fB_8CDJ2wyzUA")
+  json = JSON.parse(response.body, symbolize_names: true)
  end
 end
