6c63b100fd7f59b074b24b72a1be6781a830010f
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-11-14 10:38:34 -0700

    feat: got endpoint with incorrect params.

--- spec/facades/place_facade_spec.rb
+++ spec/facades/place_facade_spec.rb
@@ -4,14 +4,14 @@ require "rails_helper"
 RSpec.describe PlaceFacade do
  it "can return a list of recipes by country" do
   VCR.use_cassette('tourism_details') do
-   lon = 2.33
-   lat = 48.87
+   lon = "2.33"
+   lat = "48.87"
    places = PlaceFacade.tourism_details(lon, lat)
 
    places.each do |place|
-    expect(place[:properties]).to be_a(Hash)
-    expect(place[:properties]).to have_key(:name)
-    expect(place[:properties][:name]).to be_a(String)
+    expect(place.name).to be_a(String)
+    expect(place.address).to be_a(String)
+    expect(place.place_id).to be_a(String)
    end
   end
  end
--- spec/requests/api/v1/places_spec.rb
+++ spec/requests/api/v1/places_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe "Api::V1::TouristSights", type: :request do
   describe "GET /index" do
     it 'can return a list of tourist sights in each country', :vcr do
-      request_params = { lon: "2.33", lat: "48.87"}
+      request_params = {lat: "2.33", lon: "48.87"}
       
       get api_v1_tourist_sights_path, params: request_params
 
       expect(response).to be_successful
 
-      json = JSON.parse(response.body, symbolize_names: true)
-      expect(json).to be_an Hash
+      place = JSON.parse(response.body, symbolize_names: true)
+      expect(place).to be_a(Hash)
     end
   end
 end
