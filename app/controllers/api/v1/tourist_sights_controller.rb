b5b9a65dc4a75a63227326e3ad63708ba6b10daa
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-11-14 15:00:45 -0700

    retry finished with working endpoint.

--- app/controllers/api/v1/tourist_sights_controller.rb
+++ app/controllers/api/v1/tourist_sights_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::TouristSightsController < ApplicationController
 
  def index
-  country = RestFacade.country_details(params[:country])
-  @places = PlaceFacade.tourism_details(params[:lon], params[:lat])
+  country = RestFacade.get_country_lat_long(params[:country])
+  @places = PlaceFacade.tourism_details(country[1], country[0])
 
   render json: PlaceSerializer.new(@places)
  end
--- app/services/rest_service.rb
+++ app/services/rest_service.rb
@@ -10,4 +10,9 @@ class RestService
   response = conn.get("/v3.1/all")
   json = JSON.parse(response.body, symbolize_names: true)
  end
+
+ def self.get_one_country(country)
+  response = conn.get("/v3.1/name/#{country}")
+  json = JSON.parse(response.body, symbolize_names: true)
+ end
 end
