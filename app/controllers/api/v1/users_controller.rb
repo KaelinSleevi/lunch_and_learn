3833e95990c15202f1e62a32e13bd1b76edeb75c
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-11-15 11:36:49 -0700

    feat: add finished users happy path endpoint.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,11 @@
 class Api::V1::UsersController < ApplicationController
  def create
-   user = User.create(
-     name: params[:user][:name],
-     email: params[:user][:email],
-     #api_key: SecureRandom???
-   )
+   user = User.create({ 
+    name: params[:user][:name], 
+    email: params[:user][:email], 
+    api_key: SecureRandom.hex(15)
+    })
+
+   render json: UserSerializer.new(user), status: :created
  end
 end
