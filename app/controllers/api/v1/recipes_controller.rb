8364d402c5d8572d8e26965a6dc660b8698506f2
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-11-15 20:40:47 -0700

    change recipes controller to include random countries

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -2,10 +2,14 @@ class Api::V1::RecipesController < ApplicationController
 
  def index
   country = RestFacade.country_details
-  recipe_data = EdamamFacade.recipe_details(params[:country])
-  
-  render json: RecipeSerializer.new(recipe_data)
-
   random_country = RestFacade.random_country
+
+  if country.present?
+   recipe_data = EdamamFacade.recipe_details(params[:country])
+   render json: RecipeSerializer.new(recipe_data)
+  else
+   random_recipe = EdamamFacade.recipe_details(random_country)
+   render json: RecipeSerializer.new(random_recipe)
+  end
  end
 end
